c9fde5859428a1707696c78270769930c5e1e16e
tadashim <ma.tadashi@gmail.com>
2024-01-03 20:35:49 +0900

    map

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Alice"), 10);
-    scores.insert(String::from("Bob"), 20);
-    
-    for (key, value) in scores.iter() {
-        println!("{}: {}", key, value);
-    }
-}
+    let numbers = vec![1, 2, 3, 4, 5];
+    let doubled: Vec<i32> = numbers.iter().map(|x| x * 2).collect();
 
+    println!("{:?}", doubled);
+}
