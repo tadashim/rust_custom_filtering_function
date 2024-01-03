67faf4b9d0e870a7897a5e23bc9741b11f0afdc0
tadashim <ma.tadashi@gmail.com>
2024-01-03 18:46:48 +0900

    simple iterator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    let mut iter = numbers.iter();
+
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&4));
+    assert_eq!(iter.next(), Some(&5));
+    assert_eq!(iter.next(), None);
 }
